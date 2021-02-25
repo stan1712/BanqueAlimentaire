bdc0b4d661e1e1575096bf02ab484be3733da80e
Stanislas Castaybert <perso@stan1712.com>
2021-02-25 15:49:34 +0100

    ✨feature: Système de login et de register

---
+++ src/app/@test/api.service.ts
@@ -0,0 +1,68 @@
+import {
+	Injectable,
+	Output,
+	EventEmitter
+} from '@angular/core';
+import {
+	map
+} from 'rxjs/operators';
+import {
+	HttpClient
+} from '@angular/common/http';
+import {
+	Users
+} from '../../users';
+
+@Injectable({
+	providedIn: 'root'
+})
+
+export class ApiService {
+	redirectUrl: string;
+	baseUrl: string = "http://localhost/BanqueAlimentaire/php";
+
+	@Output() getLoggedInName: EventEmitter <any> = new EventEmitter();
+	constructor(private httpClient: HttpClient) {}
+	public userlogin(username, password) {
+		alert(username)
+		return this.httpClient.post <any> (this.baseUrl + '/login.php', {
+				username,
+				password
+			})
+			.pipe(map(Users => {
+				this.setToken(Users[0].name);
+				this.getLoggedInName.emit(true);
+				return Users;
+			}));
+	}
+
+	public userregistration(name, email, pwd) {
+		return this.httpClient.post <any> (this.baseUrl + '/register.php', {
+				name,
+				email,
+				pwd
+			})
+			.pipe(map(Users => {
+				console.log(Users)
+				return Users;
+			}));
+	}
+
+	//token
+	setToken(token: string) {
+		localStorage.setItem('token', token);
+	}
+	getToken() {
+		return localStorage.getItem('token');
+	}
+	deleteToken() {
+		localStorage.removeItem('token');
+	}
+	isLoggedIn() {
+		const usertoken = this.getToken();
+		if (usertoken != null) {
+			return true
+		}
+		return false;
+	}
+}
