a95c74227deed54bc355e91836ae9cf64c5410e0
Stanislas Castaybert <perso@stan1712.com>
2021-02-26 18:43:23 +0100

    ✨feature: Ajout et protection de la connexion à la base de données

---
+++ php/database.php
@@ -0,0 +1,15 @@
+<?php
+	header("Access-Control-Allow-Origin: *");
+	header('Access-Control-Allow-Credentials: true');
+	header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
+	header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+	header("Content-Type: application/json; charset=UTF-8");
+
+	require("credentials.php");
+
+	$mysqli = new mysqli($_servername, $_username, $_password, 'hostcrus_projects');
+
+	if ($mysqli->connect_error) {
+		die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
+	}
+?>
