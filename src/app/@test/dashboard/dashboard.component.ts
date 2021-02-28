6ee5235e37bc3668e7e480d12d80e1bd77d9c39c
Stanislas Castaybert <perso@stan1712.com>
2021-02-28 16:00:27 +0100

    ✨feature: Test des requêtes libres

--- src/app/@test/dashboard/dashboard.component.ts
+++ src/app/@test/dashboard/dashboard.component.ts
@@ -1,20 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../api.service';
+import { ApiService } from '../../api.service';
+import { FormGroup, FormControl, FormBuilder, Validators, NgForm } from '@angular/forms';
+import { first } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+	selector: 'app-dashboard',
+	templateUrl: './dashboard.component.html',
+	styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
-  test = this.dataService.getUser();
+	test = this.dataService.getUser();
+	result: string;
+	angForm: FormGroup;
 
-  constructor(private dataService: ApiService) { }
+	constructor(private fb: FormBuilder, private dataService: ApiService) {
+		this.angForm = this.fb.group({
+			request: ['', Validators.required]
+		});
+	}
 
-  ngOnInit(): void {
+	postdata(angForm1) {
+		this.dataService.bddRequest(angForm1.value.request).pipe(first()).subscribe(
+			data => {
+				this.result = JSON.stringify(data);
+			},
+			error => {
+				alert("qsds !")
+			});
+	}
 
-    console.log(this.test)
-  }
+	ngOnInit(): void {
+
+		console.log(this.test)
+	}
 
 }
